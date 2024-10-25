7626d545afdd049044e7dd94999c2afb528ba5d7
Osama Ech <johndoe@example.com>
2024-10-25 18:02:09 +0100

    first commmit

---
+++ Fournisseur-Service/src/main/java/org/example/fournisseurservice/FournisseurServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.fournisseurservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FournisseurServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FournisseurServiceApplication.class, args);
+    }
+
+}
---
+++ Fournisseur-Service/src/test/java/org/example/fournisseurservice/FournisseurServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.fournisseurservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FournisseurServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
