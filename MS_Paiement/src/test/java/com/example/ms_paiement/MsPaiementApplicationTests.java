591f521f87b8390e98f1919133ef784ee25dc86b
RAMI HOUSSAM <ramihoussamemsi20@gmail.com>
2024-10-26 12:27:08 +0100

    Project Architecture

---
+++ MS_Paiement/src/test/java/com/example/ms_paiement/MsPaiementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ms_paiement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPaiementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
