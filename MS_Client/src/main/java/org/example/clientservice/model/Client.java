80bbd6f1b2bac1cfa87faf9d8768bdc31350cf49
RAMI HOUSSAM <ramihoussamemsi20@gmail.com>
2024-11-02 19:14:38 +0100

    Microservice Client

---
+++ MS_Client/src/main/java/org/example/clientservice/model/Client.java
@@ -0,0 +1,25 @@
+package org.example.clientservice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String nom;
+    private String telephone;
+    private String adresse;
+    private String mail;
+}
